7b0ddda36891b62f3018db5d98ec5485ef833e40
Valerijs Krizevics <vkrizevics@gmail.com>
2018-01-05 01:11:54 +0200

    Simplifying without cloning parsers yet

--- src/XmlStringStreamer/Parser/ParserPipelinesEventDriven.php
+++ src/XmlStringStreamer/Parser/ParserPipelinesEventDriven.php
@@ -17,29 +17,14 @@ class ParserPipelinesEventDriven extends EventEmitter
     protected $parsers = array();
     
     protected $parser_pipelines = array();
-	
-	protected $event_callback = null;
     
-    protected function __construct($event_callback = null)
+    protected function __construct()
     {
-		$this->setEventCallback($event_callback);
-    }
-    
-	public function setEventCallback($event_callback = null)
-    {
-		if (is_callable($event_callback))
-		{
-			$this->event_callback = $event_callback;
-		} else {
-			$this->event_callback = null;
-		}
-		
-		return $this;
     }
 	
-    static public function getInstance($event_callback = null)
+    static public function getInstance()
     {
-	return new self($event_callback);
+	       return new self;
     }
     
     public function setParser($parser_name, ParserInterfaceEventDriven $parser)
@@ -49,8 +34,9 @@ class ParserPipelinesEventDriven extends EventEmitter
 	return $this;
     }
     
-    public function getParser($parser_name) {
-	return isset($this->parsers[$parser_name]) ? $this->parsers[$parser_name] : false;
+    public function getParser($parser_name)
+    {
+	       return isset($this->parsers[$parser_name]) ? $this->parsers[$parser_name] : false;
     }
     
     public function setParserPipeline($event_name, array $parser_names) {	
@@ -63,15 +49,15 @@ class ParserPipelinesEventDriven extends EventEmitter
 	}
     }
     
-    public function applyParserPipelines($chunk)
+    public function apply($chunk)
     {
 	$parsers = $this->parsers;
 	
 	$parser_results = 
 	    array_filter(array_map(function ($chain) use ($parsers, $chunk) {
 		$nodes = array();
-		if (!is_array($chain)) {
-		    while($nodes[] = $parsers[$chain]->getNodeFrom($chunk));
+		if (count($chain) == 1) {
+		    while($nodes[] = $parsers[current((array)$chain)]->getNodeFrom($chunk));
 		    $res = $nodes;
 		} else {
 		    $res = array_reduce($chain, function($nodes_carry, $parser_index) use ($parsers) {
@@ -86,14 +72,7 @@ class ParserPipelinesEventDriven extends EventEmitter
 
 		return $res;
 	    }, $this->parser_pipelines));
-	    
-	$that = $this;
-	$callback = is_callable($this->event_callback) ? $this->$event_callback : array($this, 'emit');
-	array_walk($parser_results, function (array $nodes, $pipeline_name) use ($callback)
-	{
-	    $callback($pipeline_name, $nodes);
-	});
 	
-	return $this;
+	return $parser_results;
     }
 }
